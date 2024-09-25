b984234b95d9965cdab4ce7d4d70382883bfdeb2
Guillermo <zeduard89>
2024-09-24 23:46:37 -0300

    introduccion a java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,5 +2,8 @@ public class HolaMundo {
     // Agregamos el metodo principal (main)
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Nombre: Guillermo Paez");
+        System.out.println("Edad: 35 años");
+        System.out.println("Pais: Argentina");
     }
 }
