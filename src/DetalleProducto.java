1c28926a065b6ebf90706dccf314921a4c791bd9
Guillermo <zeduard89>
2024-09-25 19:33:01 -0300

    variables ejercicio 1

---
+++ src/DetalleProducto.java
@@ -0,0 +1,25 @@
+public class DetalleProducto {
+    public static void main(String[] args) {
+        String nombreDelProducto = "Jamon";
+        double precio = 10.5;
+        int cantidadDisponible = 10;
+        boolean isDisponible = true;
+
+        System.out.println(
+                "Nombre del Producto: "  + nombreDelProducto + "\n" +
+                "Precio: " + precio + "\n" +
+                "Cantidad Disponible: " + cantidadDisponible + "\n" +
+                "Hay Stock: "+ isDisponible);
+
+        nombreDelProducto = "Queso";
+        precio = 20.4;
+        cantidadDisponible = 0;
+        isDisponible = false;
+
+        System.out.println(
+                "Nombre del Producto: "  + nombreDelProducto + "\n" +
+                        "Precio: " + precio + "\n" +
+                        "Cantidad Disponible: " + cantidadDisponible + "\n" +
+                        "Hay Stock: "+ isDisponible);
+    }
+}
