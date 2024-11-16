64a92bef997b57f267ee01e8da747a0bd17e0237
Guillermo <zeduard89>
2024-11-16 17:22:28 -0300

    EJB JPA

---
+++ 7)pruebaJpa2/src/main/java/test/ClienteEntidadPersona.java
@@ -0,0 +1,30 @@
+package test;
+
+import ar.com.sg.pruebajpa2.domain.Persona;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.apache.logging.log4j.*;
+
+public class ClienteEntidadPersona {
+    static Logger log = LogManager.getRootLogger();
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersonaPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        
+        //Inicia la transaccion
+        tx.begin();
+        
+        //No se debe especificar el ID de la DB
+        Persona persona1 = new Persona("Maria", "Gutierrez","mGutierrez@hotmail.com", "123456");
+        log.debug("Objeto a persisitir: " + persona1);
+        //Persistir objeto
+        em.persist(persona1);
+        //Terminamos la transaccion
+        tx.commit();
+        log.debug("Objeto persistido " + persona1);
+        em.close();
+    }
+}
